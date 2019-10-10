2e0326cbf59cb3ce8ed12d29bbb77571d5d862a4
nothing628 <yogyphang@gmail.com>
2019-10-10 11:41:52 +0700

    init commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPCoba.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
