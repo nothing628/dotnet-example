c91c983f1bd042ba6302b6804d4033f15acef1e7
nothing628 <yogyphang@gmail.com>
2019-10-14 15:54:47 +0700

    register kembali app component

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import App from './App'
 import Test from './components/Test'
 import store from './store'
 
+Vue.component('app', App)
 Vue.component('test', Test)
 Vue.config.productionTip = false
 
